7171ac0eeed42b0f6d645061f23cc760314d0fcd
wiuid <li_zhm@qq.com>
2021-04-23 10:49:56 +0800

    退出清除redis中的用户登录信息

--- webra_security/src/main/java/top/webra/hander/AjaxLogoutSuccessHandler.java
+++ webra_security/src/main/java/top/webra/hander/AjaxLogoutSuccessHandler.java
@@ -5,6 +5,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 import top.webra.bean.ResponseBean;
+import top.webra.service.impl.LogServiceImpl;
+import top.webra.util.JwtUtil;
+import top.webra.utils.RedisUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,10 +22,21 @@ public class AjaxLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Autowired
     private ResponseBean responseBean;
+    @Autowired
+    private RedisUtil redisUtil;
+    @Autowired
+    private LogServiceImpl logService;
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication)
             throws IOException {
+
+        String token = httpServletRequest.getHeader("token");
+        Long del = redisUtil.del("token" + token);
+        if (del.equals(0L)){
+            String username = JwtUtil.getUsername(token);
+            logService.createLog("退出", username, "退出操作Redis失败，退出成功");
+        }
         httpServletResponse.setContentType("application/json;charset=utf-8");
         httpServletResponse.getWriter().write(responseBean.buildOkMsg("退出成功"));
     }
