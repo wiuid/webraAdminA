27c8a82c70afbefc2c250f9fad178d2b965eba62
wiuid <li_zhm@qq.com>
2021-04-23 10:51:36 +0800

    使用新的方法解决跨域问题

--- webra_adminA/src/main/java/top/webra/config/CorsConfig.java
+++ webra_adminA/src/main/java/top/webra/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package top.webra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,14 +14,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author webra
  */
 @Configuration
-public class CorsConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .maxAge(3600);
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter () {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.addAllowedOrigin("*");
+        cors.setAllowCredentials(true);
+        cors.addAllowedHeader("*");
+        cors.addAllowedMethod("*");
+//        cors.addExposedHeader("*");
+
+        UrlBasedCorsConfigurationSource ubccs = new UrlBasedCorsConfigurationSource();
+        ubccs.registerCorsConfiguration("/**", cors);
+        return new CorsFilter(ubccs);
     }
 }
+
+//public class CorsConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("*")
+//            .allowCredentials(true)
+//            .allowedMethods("*")
+//            .allowedHeaders("*")
+//            .maxAge(3600);
+//    }
+//}
